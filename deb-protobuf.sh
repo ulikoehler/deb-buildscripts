b8cab98828338e86eb3cc601375cf20cc5ed614d
Uli Köhler <ulikoehler@online.de>
2013-04-03 16:26:43 +0200

    Stuff

--- deb-protobuf.sh
+++ deb-protobuf.sh
@@ -6,20 +6,30 @@ cd protobuf-2.5.0
 rm -rf debian
 mkdir -p debian
 #Use the LICENSE file from nodejs as copying file
-touch debian/copying
+touch debian/copyright
 #Create the changelog (no messages needed)
-dch --create -v 2.5.0-1 --package protobuf ""
+dch --create -v 2.5.0-1 --package libprotobuf ""
 #Create copyright file
 cp COPYING.txt debian/copyright
 #Create control file
-echo "Source: protobuf" > debian/control
+echo "Source: libprotobuf" > debian/control
 echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
 echo "Standards-Version: 3.9.2" >> debian/control
 echo "Build-Depends: debhelper (>= 8)" >> debian/control
+#Main library package
 echo "" >> debian/control
-echo "Package: protobuf" >> debian/control
+echo "Package: libprotobuf" >> debian/control
+echo "Architecture: amd64" >> debian/control
+echo "Provides: libprotobuf-c0, libprotobuf-c0-dev, libprotobuf-dev, libprotobuf7, libprotobuf-lite7" >> debian/control
+echo "Build-Depends: zlib1g-dev" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}, zlib1g" >> debian/control
+echo "Homepage: https://code.google.com/p/protobuf/" >> debian/control
+echo "Description: Google Protobuf library & executables (including development files)" >> debian/control
+#-dev package
+echo "" >> debian/control
+echo "Package: libprotobuf-dev" >> debian/control
 echo "Architecture: amd64" >> debian/control
 echo "Provides: libprotobuf-c0, libprotobuf-c0-dev, libprotobuf-dev, libprotobuf7, libprotobuf-lite7" >> debian/control
 echo "Build-Depends: zlib1g-dev" >> debian/control
