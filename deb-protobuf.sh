6896810106dc0b4dac06b2eaedf6d0861e728cea
Uli Köhler <ulikoehler@online.de>
2013-04-03 18:05:50 +0200

    Fix deb protobuf to compile separate dev package

--- deb-protobuf.sh
+++ deb-protobuf.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-wget https://protobuf.googlecode.com/files/protobuf-2.5.0.tar.bz2
-mv protobuf-2.5.0.tar.bz2 protobuf_2.5.0.orig.tar.bz2
-tar xjvf protobuf_2.5.0.orig.tar.bz2
-cd protobuf-2.5.0
+export VERSION=2.5.0
+export DEBVERSION=2.5.0-1
+wget https://protobuf.googlecode.com/files/protobuf-${VERSION}.tar.bz2 -O libprotobuf_${VERSION}.orig.tar.bz2
+tar xjvf libprotobuf_${VERSION}.orig.tar.bz2
+cd protobuf-${VERSION}
 rm -rf debian
 mkdir -p debian
 #Use the LICENSE file from nodejs as copying file
 touch debian/copyright
 #Create the changelog (no messages needed)
-dch --create -v 2.5.0-1 --package libprotobuf ""
+dch --create -v $DEBVERSION --package libprotobuf ""
 #Create copyright file
 cp COPYING.txt debian/copyright
 #Create control file
@@ -22,30 +23,31 @@ echo "Build-Depends: debhelper (>= 8)" >> debian/control
 echo "" >> debian/control
 echo "Package: libprotobuf" >> debian/control
 echo "Architecture: amd64" >> debian/control
-echo "Provides: libprotobuf-c0, libprotobuf-c0-dev, libprotobuf-dev, libprotobuf7, libprotobuf-lite7" >> debian/control
+echo "Provides: libprotobuf-c0, libprotobuf7, libprotobuf-lite7" >> debian/control
 echo "Build-Depends: zlib1g-dev" >> debian/control
 echo "Depends: ${shlibs:Depends}, ${misc:Depends}, zlib1g" >> debian/control
 echo "Homepage: https://code.google.com/p/protobuf/" >> debian/control
-echo "Description: Google Protobuf library & executables (including development files)" >> debian/control
+echo "Description: Google Protobuf library" >> debian/control
 #-dev package
 echo "" >> debian/control
 echo "Package: libprotobuf-dev" >> debian/control
 echo "Architecture: amd64" >> debian/control
-echo "Provides: libprotobuf-c0, libprotobuf-c0-dev, libprotobuf-dev, libprotobuf7, libprotobuf-lite7" >> debian/control
-echo "Build-Depends: zlib1g-dev" >> debian/control
-echo "Depends: ${shlibs:Depends}, ${misc:Depends}, zlib1g" >> debian/control
+echo "Provides: libprotobuf-c0, libprotobuf-c0-dev, libprotobuf-dev" >> debian/control
+echo "Depends: zlib1g, libprotobuf (= $DEBVERSION)" >> debian/control
 echo "Homepage: https://code.google.com/p/protobuf/" >> debian/control
-echo "Description: Google Protobuf library & executables (including development files)" >> debian/control
+echo "Description: Google Protobuf library & executables (development files)" >> debian/control
 #Create rules file
 echo '#!/usr/bin/make -f' > debian/rules
 echo '%:' >> debian/rules
 echo -e '\tdh $@' >> debian/rules
 echo 'override_dh_auto_configure:' >> debian/rules
-echo -e '\t./configure --with-zlib --prefix=`pwd`/debian/protobuf/usr' >> debian/rules
+echo -e '\t./configure --with-zlib --prefix=`pwd`/debian/libprotobuf/usr' >> debian/rules
 echo 'override_dh_auto_build:' >> debian/rules
 echo -e '\tmake' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
 echo -e '\tmake install' >> debian/rules
+echo -e '\tmkdir -p debian/libprotobuf-dev/usr' >> debian/rules
+echo -e '\tmv debian/libprotobuf/usr/bin debian/libprotobuf/usr/include debian/libprotobuf-dev/usr/' >> debian/rules
 #Create the target dir
 mkdir -p debian/protobuf/usr
 #Create some misc files
