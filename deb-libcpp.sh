62dc686bf27862fc700f0e0a8e569c159bda03b1
Uli Köhler <ulikoehler@online.de>
2013-04-05 02:47:09 +0200

    Add libc++

---
+++ deb-libcpp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export NAME=libcxx
+export ARCHITECTURE=amd64
+mkdir -p build/libcxx
+cd build/libcxx
+#Clone the directory
+export URL=http://llvm.org/svn/llvm-project/libcxx/trunk
+svn export $URL libcxx
+export SVNVERSION=$(svn info $URL | grep Revision | cut -d' ' -f2)
+export VERSION=0.1svn-$SVNVERSION
+export DEBVERSION=${VERSION}-1
+#Package the source
+tar cJvf libcxx_${VERSION}.tar.xz libcxx
+mv libcxx_${VERSION}.tar.xz ..
+rm -rf debian
+mkdir -p debian
+#Use the existing COPYING file
+cp libcxx/LICENSE.TXT debian/copyright
+#Create the changelog (no messages - dummy)
+dch --create -v $DEBVERSION --package ${NAME} ""
+#Create control file
+echo "Source: $NAME" > debian/control
+echo "Maintainer: None <none@example.com>" >> debian/control
+echo "Section: misc" >> debian/control
+echo "Priority: optional" >> debian/control
+echo "Standards-Version: 3.9.2" >> debian/control
+echo "Build-Depends: debhelper (>= 8), cmake" >> debian/control
+#Main library package
+echo "" >> debian/control
+echo "Package: $NAME" >> debian/control
+echo "Architecture: $ARCHITECTURE" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}" >> debian/control
+echo "Homepage: http://nodejs.org/" >> debian/control
+echo "Description: libc++ C++ Standard library" >> debian/control
+#Create rules file
+echo '#!/usr/bin/make -f' > debian/rules
+echo '%:' >> debian/rules
+echo -e '\tdh $@' >> debian/rules
+echo 'override_dh_auto_configure:' >> debian/rules
+echo -e "\tcmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/debian/${NAME}/usr libcxx" >> debian/rules
+echo 'override_dh_auto_build:' >> debian/rules
+echo -e '\tmake' >> debian/rules
+echo 'override_dh_auto_install:' >> debian/rules
+echo -e '\tmake install' >> debian/rules
+#Create some misc files
+mkdir -p debian/source
+echo "8" > debian/compat
+echo "3.0 (quilt)" > debian/source/format
+#Build it
+debuild -us -uc
