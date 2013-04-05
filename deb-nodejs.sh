931c9ec1190fe48ba0fa7d923ea383e2818407df
Uli Köhler <ulikoehler@online.de>
2013-04-05 02:17:46 +0200

    Added NodejS builder

---
+++ deb-nodejs.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+export NAME=nodejs
+export VERSION=0.10.3
+export DEBVERSION=${VERSION}-1
+export ARCHITECTURE=amd64
+export URL=http://nodejs.org/dist/v${VERSION}/node-v${VERSION}.tar.gz
+#Download it
+wget "$URL" -O ${NAME}_${VERSION}.orig.tar.gz
+tar xzvf ${NAME}_${VERSION}.orig.tar.gz
+cd node-v${VERSION}
+rm -rf debian
+mkdir -p debian
+#Use the existing COPYING file
+cp LICENSE debian/copyright
+#Create the changelog (no messages - dummy)
+dch --create -v $DEBVERSION --package ${NAME} ""
+#Create control file
+echo "Source: $NAME" > debian/control
+echo "Maintainer: None <none@example.com>" >> debian/control
+echo "Section: misc" >> debian/control
+echo "Priority: optional" >> debian/control
+echo "Standards-Version: 3.9.2" >> debian/control
+echo "Build-Depends: debhelper (>= 8)" >> debian/control
+#Main library package
+echo "" >> debian/control
+echo "Package: $NAME" >> debian/control
+echo "Architecture: $ARCHITECTURE" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}, libzmq (>= 3.0)" >> debian/control
+echo "Homepage: http://nodejs.org/" >> debian/control
+echo "Description: NodeJS" >> debian/control
+#Create rules file
+echo '#!/usr/bin/make -f' > debian/rules
+echo '%:' >> debian/rules
+echo -e '\tdh $@' >> debian/rules
+echo 'override_dh_auto_configure:' >> debian/rules
+echo -e "\t./configure --prefix=`pwd`/debian/${NAME}/usr" >> debian/rules
+echo 'override_dh_auto_build:' >> debian/rules
+echo -e '\tmake' >> debian/rules
+echo 'override_dh_auto_install:' >> debian/rules
+echo -e '\tmake install' >> debian/rules
+#Create some misc files
+mkdir -p debian/source
+echo "8" > debian/compat
+echo "3.0 (quilt)" > debian/source/format
+#Build it
+debuild -us -uc
