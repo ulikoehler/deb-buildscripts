c6f555e134c5983f766bbe385c18dbe734e8655b
Uli Köhler <ulikoehler@online.de>
2013-04-05 03:54:25 +0200

    Fix libcxx

--- deb-libcpp.sh
+++ deb-libcpp.sh
@@ -24,24 +24,33 @@ echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
 echo "Standards-Version: 3.9.2" >> debian/control
-echo "Build-Depends: debhelper (>= 8), cmake, clang (>= 3.0)" >> debian/control
+echo "Build-Depends: debhelper (>= 8), cmake, clang-3.2" >> debian/control
 #Main library package
 echo "" >> debian/control
 echo "Package: $NAME" >> debian/control
 echo "Architecture: $ARCHITECTURE" >> debian/control
 echo "Depends: ${shlibs:Depends}, ${misc:Depends}" >> debian/control
-echo "Homepage: http://nodejs.org/" >> debian/control
+echo "Homepage: http://libcxx.llvm.org/" >> debian/control
+echo "Description: libc++ C++ Standard library" >> debian/control
+#Main library package
+echo "" >> debian/control
+echo "Package: $NAME-dev" >> debian/control
+echo "Architecture: any" >> debian/control
+echo "Depends: $NAME (= $DEBVERSION)" >> debian/control
+echo "Homepage: http://libcxx.llvm.org/" >> debian/control
 echo "Description: libc++ C++ Standard library" >> debian/control
 #Create rules file
 echo '#!/usr/bin/make -f' > debian/rules
 echo '%:' >> debian/rules
 echo -e '\tdh $@' >> debian/rules
 echo 'override_dh_auto_configure:' >> debian/rules
-echo -e "\tcmake -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/debian/${NAME}/usr libcxx" >> debian/rules
+echo -e "\tcmake -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX:PATH=`pwd`/debian/${NAME}/usr libcxx" >> debian/rules
 echo 'override_dh_auto_build:' >> debian/rules
 echo -e '\tmake' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
 echo -e '\tmake install' >> debian/rules
+echo -e "\tmkdir -p debian/$NAME-dev/usr" >> debian/rules
+echo -e "\tmv debian/$NAME/usr/include debian/$NAME-dev/usr" >> debian/rules
 #Create some misc files
 mkdir -p debian/source
 echo "8" > debian/compat
