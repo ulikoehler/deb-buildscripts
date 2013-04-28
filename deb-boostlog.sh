ec43faa685baa3081784d97edaa4bcef67a10a8f
Uli Köhler <ulikoehler@online.de>
2013-04-28 15:47:17 +0200

    Fix boostlog

--- deb-boostlog.sh
+++ deb-boostlog.sh
@@ -24,7 +24,7 @@ echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
 echo "Standards-Version: 3.9.2" >> debian/control
-echo "Build-Depends: debhelper (>= 8)" >> debian/control
+echo "Build-Depends: debhelper (>= 8), boost-build (>= 1.50.0)" >> debian/control
 echo "" >> debian/control
 echo "Package: boost-all" >> debian/control
 echo "Architecture: amd64" >> debian/control
@@ -37,12 +37,12 @@ echo -e '\tdh $@' >> debian/rules
 echo 'override_dh_auto_configure:' >> debian/rules
 echo -e '\t' >> debian/rules
 echo 'override_dh_auto_build:' >> debian/rules
-echo -e '\t./b2 --prefix=`pwd`/debian/boost-all/usr/' >> debian/rules
+echo -e '\tb2 --prefix=`pwd`/debian/boost-all/usr/' >> debian/rules
 echo 'override_dh_auto_test:' >> debian/rules
 echo -e '\t' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
 echo -e '\tmkdir -p debian/boost-all/usr' >> debian/rules
-echo -e '\t./b2 --prefix=`pwd`/debian/boost-all/usr/ install' >> debian/rules
+echo -e '\tb2 --prefix=`pwd`/debian/boost-all/usr/ install' >> debian/rules
 #Create some misc files
 echo "8" > debian/compat
 mkdir -p debian/source
