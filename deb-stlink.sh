4e357e4810e022668abb1df6853e4242ca7b62e2
Uli Köhler <ulikoehler@online.de>
2013-05-22 16:40:04 +0200

    Add STLink udev scripts

--- deb-stlink.sh
+++ deb-stlink.sh
@@ -2,11 +2,11 @@
 export NAME=stlink
 git clone git://github.com/texane/stlink.git
 export VERSION=1.0-git$(git rev-parse HEAD | cut -c1-10)
-export DEBVERSION=${VERSION}-1
+export DEBVERSION=${VERSION}-2
 cd stlink
 git pull
 rm -rf debian
-mkdir -p debian
+mkdir -p debian/$NAME
 #Use the LICENSE file from nodejs as copying file
 touch debian/copyright
 #Create the changelog (no messages needed)
@@ -19,13 +19,13 @@ echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
 echo "Standards-Version: 3.9.2" >> debian/control
-echo "Build-Depends: debhelper (>= 8)" >> debian/control
+echo "Build-Depends: debhelper (>= 8), libusb-1.0-0-dev" >> debian/control
 #Main library package
 echo "" >> debian/control
 echo "Package: stlink" >> debian/control
 echo "Architecture: amd64" >> debian/control
 echo "Provides: st-flash" >> debian/control
-echo "Depends: ${shlibs:Depends}, ${misc:Depends}" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}, udev" >> debian/control
 echo "Homepage: https://github.com/texane/stlink/" >> debian/control
 echo "Description: Texane STLink Linux port" >> debian/control
 #Create rules file
@@ -38,6 +38,9 @@ echo -e "\t./configure --prefix=`pwd`/debian/$NAME/usr" >> debian/rules
 echo 'override_dh_auto_build:' >> debian/rules
 echo -e '\tmake' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
+echo -e "\tmkdir -p debian/stlink/etc/udev/rules.d" >> debian/rules
+echo -e "\tcp 49-stlinkv1.rules debian/$NAME/etc/udev/rules.d/" >> debian/rules
+echo -e "\tcp 49-stlinkv2.rules debian/$NAME/etc/udev/rules.d/" >> debian/rules
 echo -e '\tmake install' >> debian/rules
 #Create the target dir
 mkdir -p debian/$NAME/usr
