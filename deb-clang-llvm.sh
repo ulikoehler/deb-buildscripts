84ce31bdb75c39102b4d820011c3c915525d4a84
Uli Köhler <ulikoehler@online.de>
2013-06-20 14:19:40 +0200

    Bump LLVM to 3.3

--- deb-clang-llvm.sh
+++ deb-clang-llvm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export NAME=clang+llvm
-export VERSION=3.2
+export VERSION=3.3
 export DEBVERSION=${VERSION}-1
-wget http://llvm.org/releases/3.2/clang+llvm-3.2-x86_64-linux-ubuntu-12.04.tar.gz -O ${NAME}_-3.2.orig.tar.gz
-tar xzvf ${NAME}_-3.2.orig.tar.gz
-cd ${NAME}-3.2-x86_64-linux-ubuntu-12.04
+wget http://llvm.org/releases/3.3/clang+llvm-3.3-amd64-Ubuntu-12.04.2.tar.gz
+tar xzvf clang+llvm-3.3-amd64-Ubuntu-12.04.2.tar.gz
+cd clang+llvm-3.3-amd64-Ubuntu-12.04.2.tar.gz
 rm -rf debian
 mkdir -p debian
 #Use the LICENSE file from nodejs as copying file
@@ -12,7 +12,7 @@ touch debian/copying
 #Create the changelog (no messages needed)
 dch --create -v $DEBVERSION --package $NAME ""
 #Create control file
-echo "Source: clang+llvm" > debian/control
+echo "Source: $NAME" > debian/control
 echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
@@ -21,7 +21,7 @@ echo "Build-Depends: debhelper (>= 8)" >> debian/control
 echo "" >> debian/control
 echo "Package: $NAME" >> debian/control
 echo "Architecture: amd64" >> debian/control
-echo "Provides: llvm, llvm-3.2, llvm-3.2-dev, llvm-dev, libllvm-3.2, libllvm-3.2-dev, clang, clang-3.2, clang-3.2-doc" >> debian/control
+echo "Provides: llvm, llvm-3.3, llvm-3.3-dev, llvm-dev, libllvm-3.3, libllvm-3.3-dev, clang, clang-3.3, clang-3.3-doc" >> debian/control
 echo "Depends: ${shlibs:Depends}, ${misc:Depends}" >> debian/control
 echo "Description: Vanilla LLVM + Clang distribution" >> debian/control
 #Create rules file
@@ -35,8 +35,8 @@ echo -e '\t' >> debian/rules
 echo 'override_dh_auto_test:' >> debian/rules
 echo -e '\t' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
-echo -e '\tmkdir -p debian/clang+llvm/usr' >> debian/rules
-echo -e '\tcp -r lib include bin share debian/clang+llvm/usr' >> debian/rules
+echo -e "\tmkdir -p debian/$NAME/usr" >> debian/rules
+echo -e "\tcp -r lib include bin share debian/$NAME/usr" >> debian/rules
 #Create some misc files
 echo "8" > debian/compat
 mkdir -p debian/source
