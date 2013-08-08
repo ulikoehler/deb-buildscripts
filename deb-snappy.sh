8780139113532c14c7528cac36d6ec5ee3f8ebbd
Uli Köhler <ulikoehler@online.de>
2013-08-08 03:32:24 +0200

    Fix snappy

--- deb-snappy.sh
+++ deb-snappy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export NAME=libsnappy
+export NAME=lib snappy
 export VERSION=1.1.0
 export DEBVERSION=${VERSION}-1
 export URL=https://snappy.googlecode.com/files/snappy-1.1.0.tar.gz
@@ -21,12 +21,12 @@ echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
 echo "Standards-Version: 3.9.2" >> debian/control
-echo "Build-Depends: debhelper (>= 8), libjemalloc-dev" >> debian/control
+echo "Build-Depends: debhelper (>= 8)" >> debian/control
 #Main library package
 echo "" >> debian/control
-echo "Package: ${NAME}1" >> debian/control
+echo "Package: libsnappy1" >> debian/control
 echo "Architecture: all" >> debian/control
-echo "Provides: libsnappy" >> debian/control
+echo "Provides: libsnappy1" >> debian/control
 echo "Depends: ${shlibs:Depends}, ${misc:Depends}, libjemalloc1" >> debian/control
 echo "Homepage: https://code.google.com/p/snappy/" >> debian/control
 echo "Description: Snappy compression library" >> debian/control
@@ -43,13 +43,14 @@ echo '#!/usr/bin/make -f' > debian/rules
 echo '%:' >> debian/rules
 echo -e '\tdh $@' >> debian/rules
 echo 'override_dh_auto_configure:' >> debian/rules
-echo -e "\t./configure --prefix=`pwd`/debian/${NAME}/usr" >> debian/rules
+echo -e "\t./configure --prefix=`pwd`/debian/libsnappy1/usr" >> debian/rules
 echo 'override_dh_auto_build:' >> debian/rules
 echo -e '\tmake -j8' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
+echo -e "\tmkdir -p debian/libsnappy1/usr" >> debian/rules
 echo -e '\tmake install' >> debian/rules
 echo -e "\tmkdir -p debian/$NAME-dev/usr" >> debian/rules
-echo -e "\tmv debian/$NAME/usr/include debian/$NAME-dev/usr/" >> debian/rules
+echo -e "\tmv debian/libsnappy1/usr/include debian/$NAME-dev/usr/" >> debian/rules
 #Create some misc files
 mkdir -p debian/source
 echo "8" > debian/compat
