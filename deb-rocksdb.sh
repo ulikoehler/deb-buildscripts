e2a00e8041ed0a764db649eba46924d4081d145b
Uli Köhler <ulikoehler@online.de>
2013-12-09 04:34:52 +0100

    Fix rocksdb dependencies

--- deb-rocksdb.sh
+++ deb-rocksdb.sh
@@ -5,7 +5,7 @@
 git clone git://github.com/facebook/rocksdb.git
 cd rocksdb
 export VERSION=1.0-git$(git rev-list --all | wc -l)
-export DEBVERSION=${VERSION}-1
+export DEBVERSION=${VERSION}-2
 rm -rf debian
 mkdir -p debian
 #Use the existing LICENSE file
@@ -18,13 +18,13 @@ echo "Maintainer: None <none@example.com>" >> debian/control
 echo "Section: misc" >> debian/control
 echo "Priority: optional" >> debian/control
 echo "Standards-Version: 3.9.2" >> debian/control
-echo "Build-Depends: debhelper (>= 8), libsnappy-dev (>= 1.0), libgflags-dev, libjemalloc-dev" >> debian/control
+echo "Build-Depends: debhelper (>= 8), libsnappy-dev (>= 1.0), libgflags-dev, libjemalloc-dev, libbz2-dev, zlib1g-dev" >> debian/control
 #Create the library package
 echo "" >> debian/control
 echo "Package: librocksdb" >> debian/control
 echo "Version: $DEBVERSION" >> debian/control
 echo "Architecture: any" >> debian/control
-echo "Depends: ${shlibs:Depends}, ${misc:Depends}, libsnappy1 (>= 1.0), libjemalloc1" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}, libsnappy1 (>= 1.0), libjemalloc1, libbz2-1.0, zlib1g" >> debian/control
 echo "Homepage: http://rocksdb.org/" >> debian/control
 echo "Description: RocksDB Key-Value database" >> debian/control
 #Also create the -dev package
@@ -32,7 +32,7 @@ echo "" >> debian/control
 echo "Package: librocksdb-dev" >> debian/control
 echo "Version: $DEBVERSION" >> debian/control
 echo "Architecture: any" >> debian/control
-echo "Depends: libsnappy-dev (>= 1.0), libleveldb (>= ${VERSION})" >> debian/control
+echo "Depends: libsnappy-dev (>= 1.0), librocksdb (>= ${VERSION})" >> debian/control
 echo "Homepage: http://rocksdb.org/" >> debian/control
 echo "Description: RocksDB Key-Value database (development files)" >> debian/control
 #Create rules file
@@ -41,10 +41,10 @@ echo '%:' >> debian/rules
 echo -e '\tdh $@' >> debian/rules
 echo 'override_dh_auto_configure:' >> debian/rules
 echo -e '\tsed -i "s/tcmalloc/jemalloc/g" build_tools/build_detect_platform' >> debian/rules
-#Compatibility for clang++ (g++ has some compatibility issues
-echo -e '\tsed -i "s/-fno-builtin-memcmp//g" build_tools/build_detect_platform' >> debian/rules
+#Compatibility for older g++
+echo -e '\tsed -i "s/gnu++11/gnu++0x/g" build_tools/build_detect_platform' >> debian/rules
 echo 'override_dh_auto_build:' >> debian/rule
-echo -e '\tCXX="clang++" CCFLAGS="-march=core2" CXXFLAGS="-march=core2" make all librocksdb.so' >> debian/rules
+echo -e '\tCCFLAGS="-march=core2" CXXFLAGS="-march=core2" make all librocksdb.so' >> debian/rules
 echo 'override_dh_auto_test:' >> debian/rules
 echo -e '\t' >> debian/rules
 echo 'override_dh_auto_install:' >> debian/rules
