937b79b39db5ea84e910977b51f8349e60c32961
Uli Köhler <ulikoehler@online.de>
2014-05-20 01:01:41 +0200

    Fix versions

--- deb-czmq.sh
+++ deb-czmq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export NAME=libczmq
 export VERSION=2.2.0
-export DEBVERSION=${VERSION}-1
+export DEBVERSION=${VERSION}-2
 export URL=http://download.zeromq.org/czmq-${VERSION}.tar.gz
 #Download it
 wget "$URL" -O ${NAME}_${VERSION}.orig.tar.gz
