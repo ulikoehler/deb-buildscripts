fa860ff6014766b8eefefa95c98fbf7ecb6a4380
Uli Köhler <ulikoehler@online.de>
2014-07-20 04:27:28 +0200

    Bump LLVM to 3.4.2

--- deb-clang-llvm.sh
+++ deb-clang-llvm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export NAME=clang+llvm
-export VERSION=3.4.1
+export VERSION=3.4.2
 export DEBVERSION=${VERSION}-1
-wget http://llvm.org/releases/3.4.1/clang+llvm-3.4.1-x86_64-unknown-ubuntu12.04.tar.xz
-tar xJvf clang+llvm-3.4.1-x86_64-unknown-ubuntu12.04.tar.xz
-cd clang+llvm-3.4.1-x86_64-unknown-ubuntu12.04
+export DIRNAME=clang+llvm-${VERSION}-x86_64-unknown-ubuntu14.04
+export FILENAME=${DIRNAME}.tar.xz
+wget http://llvm.org/releases/${VERSION}/$FILENAME
+tar xJvf $FILENAME
+cd $DIRNAME
 rm -rf debian
 mkdir -p debian
 #Use the LICENSE file from nodejs as copying file
