a1fcb705d70f7f57fd56ce84930b787ff28ae5d4
Uli Köhler <ulikoehler@online.de>
2015-04-07 22:58:23 +0200

    Added AFL builder

---
+++ deb-afl.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+export NAME=afl
+export VERSION=1.59b
+export DEBVERSION=${VERSION}-1
+export URL=http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz
+#Download it
+wget "$URL" -O ${NAME}_${VERSION}.orig.tar.gz
+tar xzvf ${NAME}_${VERSION}.orig.tar.gz
+cd afl-${VERSION}
+rm -rf debian
+mkdir -p debian
+#Use the existing COPYING file
+cp COPYING debian/copyright
+#Create the changelog (no messages - dummy)
+dch --create -v $DEBVERSION --package ${NAME} ""
+#Create copyright file
+cp COPYING debian/copyright
+#Create control file
+echo "Source: $NAME" > debian/control
+echo "Maintainer: None <none@example.com>" >> debian/control
+echo "Section: misc" >> debian/control
+echo "Priority: optional" >> debian/control
+echo "Standards-Version: 3.9.2" >> debian/control
+echo "Build-Depends: debhelper (>= 8)" >> debian/control
+#Main library package
+echo "" >> debian/control
+echo "Package: $NAME" >> debian/control
+echo "Architecture: any" >> debian/control
+echo "Depends: ${shlibs:Depends}, ${misc:Depends}" >> debian/control
+echo "Homepage: http://lcamtuf.coredump.cx/afl/" >> debian/control
+echo "Description: American Fuzzy Lop (AFL)" >> debian/control
+#Create rules file
+echo '#!/usr/bin/make -f' > debian/rules
+echo '%:' >> debian/rules
+echo -e '\tdh $@' >> debian/rules
+echo 'override_dh_auto_configure:' >> debian/rules
+echo -e '\t' >> debian/rules
+echo 'override_dh_auto_build:' >> debian/rules
+echo -e '\tmake -j8' >> debian/rules
+echo 'override_dh_auto_install:' >> debian/rules
+echo -e "\tPREFIX=/usr DESTDIR=`pwd`/debian/$NAME make install" >> debian/rules
+#Create some misc files
+mkdir -p debian/source
+echo "8" > debian/compat
+echo "3.0 (quilt)" > debian/source/format
+#Build it
+debuild -us -uc
