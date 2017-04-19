2e155627d758619e1483906b10d6a937a6d8145d
Uli Köhler <ulikoehler@online.de>
2017-04-19 21:22:27 +0200

    More rocksdb fixes

--- deb-rocksdb.py
+++ deb-rocksdb.py
@@ -5,7 +5,11 @@ set_name("librocksdb")
 set_homepage("http://rocksdb.org/")
 #Download it
 pkgversion = "5.2.1"
+<<<<<<< 0ed6e534a57d347c4106e479bd875eda35ebe477
 set_version(pkgversion + "-deb5")
+=======
+set_version(pkgversion + "-deb4")
+>>>>>>> More rocksdb fixes
 git_clone("https://github.com/facebook/rocksdb.git", branch="v" + pkgversion, depth=1)
 set_debversion(1)
 # Remove git
@@ -20,19 +24,20 @@ create_dummy_changelog()
 
 instcmd = "INSTALL_PATH=debian/{}/usr make install-shared".format(get_name())
 # Create rules file
-build_config_autotools(targets=["shared_lib"], install_cmd=instcmd)
+build_config_autotools(targets=[""], install_cmd=instcmd)
 install_usr_dir_to_package("usr/include", "dev")
 build_config["test"] = []
 build_config["clean"] = []
+build_config["build"] = ["PORTABLE=1 CFLAGS=-march=corei7 make -j4 shared_lib"]
 build_config["configure"] = []
 write_rules()
 
-build_depends += ["zlib1g-dev", "libbz2-dev", "libsnappy-dev"]
+build_depends += ["zlib1g-dev", "libbz2-dev", "libsnappy-dev", "libgflags-dev"]
 
 #Create control file
 intitialize_control()
 control_add_package(
-    depends=["libbz2-1.0", "libsnappy1v5 (>= 1.0)", "zlib1g"],
+    depends=["libbz2-1.0", "libsnappy1v5 (>= 1.0)", "zlib1g", "libgflags2v5"],
     description="RocksDB Key-Value database")
 
 control_add_package("dev",
