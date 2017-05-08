db1ca6b00b7a0a350f664c005b600a81973fe607
Uli Köhler <ulikoehler@online.de>
2017-05-09 00:48:12 +0200

    Try to fix python builder

--- deblib.py
+++ deblib.py
@@ -282,7 +282,8 @@ def build_config_python(python="python3"):
     """
     global build_depends
     build_config["clean"] = []
-    build_config["build"] = [
+    build_config["build"] = []
+    build_config["install"] = [
         "{} setup.py install --prefix=debian/{}/usr".format(
             python, get_name())
     ]
