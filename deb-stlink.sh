b67b443fc0da70e1ad60ec44d5e028eaeba221cc
Uli Köhler <ukoehler@techoverflow.net>
2020-05-04 01:21:10 +0200

    Added deb stlink

--- deb-stlink.sh
+++ deb-stlink.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export NAME=stlink
-git clone git://github.com/texane/stlink.git --depth 1 -b 1.4.0
-export VERSION=1.4.0
+git clone git://github.com/stlink-org/stlink.git --depth 1 -b v1.6.0
+export VERSION=1.6.0
 export DEBVERSION=${VERSION}-1
 cd stlink
 git pull
