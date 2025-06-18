3d4efde25d8bb446b2a352c5302b5ec3aba0f6e2
Uli Köhler <ukoehler@techoverflow.net>
2025-06-18 02:50:09 +0200

    Fix open62541 uploader

--- upload-open62541.sh
+++ upload-open62541.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Usage: upload.sh <.changes file>
 debsign -k 1B69364900A773C4319949E805A0300DAE58DEBD libopen62541_*.changes
-dput ppa:ulikoehler/rapidjson-update libopen62541_*changes
+dput ppa:ulikoehler/deb-buildscripts libopen62541_*changes
